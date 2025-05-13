198c2e72030e920d8dc6da16bb9dd0fa564b948a
Colton Shaw <coltoneshaw@gmail.com>
2025-05-13 00:53:45 -0400

    refactor: improve flight data formatting and logging in flight service

--- flightaware_app/flight_service.go
+++ flightaware_app/flight_service.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 	"strings"
 	"time"
@@ -12,9 +13,9 @@ import (
 // getDepartureFlights fetches flights departing from the specified airport
 func getDepartureFlights(airport string, start, end int64) (*DepartureFlights, error) {
 	// Construct the API URL
-	apiURL := fmt.Sprintf("https://opensky-network.org/api/flights/departure?airport=%s&begin=%d&end=%d", 
+	apiURL := fmt.Sprintf("https://opensky-network.org/api/flights/departure?airport=%s&begin=%d&end=%d",
 		strings.ToUpper(airport), start, end)
-	
+
 	log.Printf("Requesting flights from: %s", apiURL)
 
 	// Create HTTP client with timeout
@@ -66,22 +67,22 @@ func formatFlightResponse(flights *DepartureFlights, airport string, start, end 
 	var sb strings.Builder
 	sb.WriteString(fmt.Sprintf("**Departures from %s**\n", airport))
 	sb.WriteString(fmt.Sprintf("Time range: %s to %s\n\n", startTime, endTime))
-	
+
 	// Limit to 20 flights to avoid message size limits
 	maxFlights := 20
 	if len(flights.Flights) < maxFlights {
 		maxFlights = len(flights.Flights)
 	}
-	
+
 	for i := 0; i < maxFlights; i++ {
 		flight := flights.Flights[i]
 		departureTime := time.Unix(flight.FirstSeen, 0).Format("15:04 MST")
-		
+
 		// Clean up callsign (remove trailing spaces)
 		callsign := strings.TrimSpace(flight.Callsign)
-		
+
 		sb.WriteString(fmt.Sprintf("- **%s**: Departed at %s", callsign, departureTime))
-		
+
 		// Add departure airport
 		departureCode := flight.EstDepartureAirport
 		// Try to convert ICAO code to more recognizable 3-letter code if possible
@@ -91,7 +92,7 @@ func formatFlightResponse(flights *DepartureFlights, airport string, start, end 
 				break
 			}
 		}
-		
+
 		// Add destination if available
 		if flight.EstArrivalAirport != "" {
 			destinationCode := flight.EstArrivalAirport
@@ -104,20 +105,20 @@ func formatFlightResponse(flights *DepartureFlights, airport string, start, end 
 			}
 			sb.WriteString(fmt.Sprintf(" to %s", destinationCode))
 		}
-		
+
 		// Add flight duration if available
 		if flight.LastSeen > flight.FirstSeen {
 			duration := flight.LastSeen - flight.FirstSeen
 			minutes := duration / 60
 			sb.WriteString(fmt.Sprintf(", Duration: %d min", minutes))
 		}
-		
+
 		sb.WriteString("\n")
 	}
-	
+
 	if len(flights.Flights) > maxFlights {
 		sb.WriteString(fmt.Sprintf("\n_Showing %d of %d total flights_", maxFlights, len(flights.Flights)))
 	}
-	
+
 	return sb.String()
 }
